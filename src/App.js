a362baecc8bfc83571914cb19e2b5b54b2987b9e
lautaro.bazalo <lautaro.bazalo@alu.ing.unlp.edu.ar>
2022-11-20 00:46:19 -0300

    adding style

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const host='http://192.168.1.38'
 
   const [numState,setState]=useState({
-    num:""
+    num:" "
   })
   
   const handleChange= (event)=> {
@@ -16,8 +16,16 @@ function App() {
   }
   
   const handleSubmit =(event)=> {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
+    let endpoint= "/powerWithDelay?delay=" + numState.num
+    console.log("el endpoint es: "+endpoint)
+    fetch(endpoint,{
+      referrer:host
+    })
+    .then(response=>console.log(response))
+    .then(alert("Prendido por "+numState.num + " minutos"))
+    .catch(err=>console.log(err))
+  
+   
   }
   
   const onPower=()=>{
@@ -50,15 +58,15 @@ function App() {
       <button type="button" className="btn btn-danger" onClick={onPower}>Prender</button>
     </li>
     <li>
-      <button type="button" className="btn btn-dark" onClick={offBomb}>Apagar</button>
+      <button type="button" className="btn btn-light" onClick={offBomb}>Apagar</button>
     </li>    
-    <li>
-
-    <form onSubmit={handleSubmit}>
+    
+    <form onSubmit={handleSubmit} className='flex-column'>
       <label>Prender durante: </label>
-      <input type="number" className='mt-3 mb-1' min={0} value={numState.num} onChange={handleChange}></input>
+      <input type="number"  min={0} value={numState.num} onChange={handleChange}></input>
+      <button type="submit" name="quantity" className="btn btn-danger">Prender</button>
     </form>
-    </li>
+    
      </ul> 
        
     </div>
